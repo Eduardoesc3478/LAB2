d6cebbe2d3bc4bb30d133b0954e2d0229c0e0d1c
Informatica <informatica@alumno.local>
2025-02-07 12:02:32 -0600

    proyecto

---
+++ src/middlewares/appointment-validators.js
@@ -0,0 +1,17 @@
+import { body } from "express-validator";
+import { validarCampos } from "./validate-fields.js";
+import { handleErrors } from "./handle-errors.js";
+
+export const createAppointmentValidator = [
+    body("date").notEmpty().withMessage("La fecha es requerida"),
+    body("pet").notEmpty().withMessage("La mascota es requerida"),
+    body("pet").isMongoId().withMessage("No es un ID válido de MongoDB"),
+    validarCampos,
+    handleErrors
+];
+export const cancelAppointmentValidator = [
+    param("id").isMongoId().withMessage("No es un ID válido de MongoDB"),
+    param("id").custom(appointmentExists),
+    validarCampos,
+    handleErrors
+]
